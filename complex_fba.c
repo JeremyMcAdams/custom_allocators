3cfa85fa8a800d3ac56cae17ef7bfc8d013147ca
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-11-11 23:31:24 -0500

    got basic first fit allocation solved

--- complex_fba.c
+++ complex_fba.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #define MEM_OFFSET 5
 #define HEADER_SIZE 4
@@ -68,14 +69,7 @@ uint32_t fba_get_size(FixedAllocator* restrict fba, void* mem) {
 
 static uint32_t* increment_header(FixedAllocator* restrict fba, uint32_t* last_header) {
     uint32_t* next_header = (void*) last_header + align_next_header(last_header);
-//    if (is_allocated(next_header) == true && (void*) next_header + align_next_header(next_header) >= fba->buffer + fba->cur_ptr) { 
-//        return 0;
-//    }
-//    else if (is_allocated(next_header) == true && (void*) next_header + align_next_header(next_header) >= fba->buffer + fba->end_ptr) {
-//        return 0;
-//    }
     return next_header;
-    
 }
 
 static void merge_free_blocks(uint32_t* header1, uint32_t* header2) {
@@ -84,23 +78,40 @@ static void merge_free_blocks(uint32_t* header1, uint32_t* header2) {
     *header2 = 0;
     *header1 = new_size << 3;
 }
-static void* fba_find_mem(FixedAllocator* restrict fba, unsigned bytes) {
+static uint32_t* fba_find_mem(FixedAllocator* restrict fba, unsigned bytes) {
     uint32_t* last_header = fba->buffer;
     uint32_t* current_header = increment_header(fba, last_header); 
-    //is mostly working but double check the header merge math checks out
+    uint32_t* return_header = 0;
     while ((void*) current_header < fba->buffer + fba->cur_ptr && (void*) current_header < fba->buffer + fba->end_ptr) {
-        printf("*last_header    = 0x%x\n", *last_header);
-        printf("*current_header = 0x%x\n", *current_header);
-        if (is_allocated(current_header) == false && is_allocated(last_header) == false) {
-            merge_free_blocks(last_header, current_header);
-            printf("last_header new size = %lu\n", *last_header >> 3);
-            current_header = increment_header(fba, last_header);
+        if (is_allocated(last_header) == false && (*last_header >> 3) >= bytes) {
+            return_header = last_header;
+            break;
         }
         last_header = current_header;
         current_header = increment_header(fba, last_header);
-        
     }
-    return 0;
+    if (return_header == 0 && is_allocated(current_header) == false && (*current_header >> 3)) {
+        return_header = current_header;
+    }
+    if (return_header == 0) { //no block free
+        return 0;
+    }
+
+    uint32_t header_size = *return_header >> 3;
+    if (header_size - bytes < 4) {
+        *return_header = (bytes << 3) + (((header_size - bytes) & 3) << 1) + 1;
+    }
+    else if (header_size - bytes == 4) {
+        *return_header = (bytes << 3) + 1; 
+        uint32_t* new_header = increment_header(fba, return_header);
+        *new_header = 0;
+    }
+    else {
+        *return_header = (bytes << 3) + 1;
+        uint32_t* new_header = increment_header(fba, return_header);
+        *new_header = (header_size - bytes) << 3;
+    }
+    return return_header;
 }
 
 void* fba_malloc(FixedAllocator* restrict fba, const unsigned bytes) {
@@ -108,7 +119,11 @@ void* fba_malloc(FixedAllocator* restrict fba, const unsigned bytes) {
         return 0;
     }
     else if (fba->cur_ptr + HEADER_SIZE + bytes > fba->end_ptr) {
-        void* return_pointer = fba_find_mem(fba, bytes);
+        uint32_t* return_header = fba_find_mem(fba, bytes);
+        if (return_header) {
+            printf("fba_malloc return_header = 0x%x\n", *return_header); 
+        }
+        void* return_pointer = ((void*) return_header) + MEM_OFFSET;
         return return_pointer;
     } 
     else {
@@ -133,32 +148,50 @@ void fba_free(FixedAllocator* restrict fba, void* mem) {
             printf("%u\n", size % 8);
             switch (size % 8) {
                 case 0:
+                    puts("freeing at 0");
                     for (uint64_t* byte = (uint64_t*) mem; byte < (uint64_t*) ((void*) mem + size); byte++) {
                         *byte = 0;
                     }
                     break;
                 case 2:
+                    puts("freeing at 2");
                     for (uint16_t* byte = (uint16_t*) mem; byte < (uint16_t*) ((void*) mem + size); byte++) {
                         *byte = 0;
                     }
                     break;
                 case 4:
+                    puts("freeing at 4");
                     for (uint32_t* byte = (uint32_t*) mem; byte < (uint32_t*) ((void*) mem + size); byte++) {
                         *byte = 0;
                     }
                     break;
                 case 6:
+                    puts("freeing at 6");
                     for (uint16_t* byte = (uint16_t*) mem; byte < (uint16_t*) ((void*) mem + size); byte++) {
                         *byte = 0;
                     }
                     break;
                 default:
+                    puts("freeing at default");
                     for (uint8_t* byte = (uint8_t*) mem; byte < (uint8_t*) ((void*) mem + size); byte++) {
                         *byte = 0;
                     }
                     break;
             }
             *header -= 1; //turns off allocation bit
+            header = fba->buffer;
+            uint32_t* next_header = increment_header(fba, header);
+            while ((void*) next_header < fba->buffer + fba->cur_ptr && (void*) next_header < fba->buffer + fba->end_ptr) {
+                printf("*header    = 0x%x\n", *header);
+                printf("*next_header = 0x%x\n", *next_header);
+                if (is_allocated(next_header) == false && is_allocated(header) == false) {
+                    merge_free_blocks(header, next_header);
+                    printf("last_header new size = %lu\n", *header >> 3);
+                    next_header = increment_header(fba, header);
+                }
+                header = next_header;
+                next_header = increment_header(fba, header);
+            }
         }
     }
 }
@@ -166,22 +199,36 @@ void fba_free(FixedAllocator* restrict fba, void* mem) {
 int main() {
     uint8_t buffer[100] = {0};
     FixedAllocator fba = init_fba(buffer, sizeof(buffer));
-    int* array = fba_malloc(&fba, 10 * sizeof(int));
-    short* array2 = fba_malloc(&fba, 10 * sizeof(short));
-    printf("array2 size: %u\n", fba_get_size(&fba, array2));
-    printf("array size: %u\n", fba_get_size(&fba, array));
-    printf("buffer = %lu\n", buffer);
-    printf("array = %lu\n", array);
-    printf("array2 = %lu\n", array2);
-//    int* array3 = fba_malloc(&fba, 11 * sizeof(int));
-    printf("*array = 0x%x\n", *((uint32_t*)((void*) array - MEM_OFFSET)));
-    printf("*array2 = 0x%x\n", *((uint32_t*)((void*) array2 - MEM_OFFSET)));
-    fba_free(&fba, array);
-    fba_free(&fba, array2);
-    char* string = fba_malloc(&fba, 11 * sizeof(char));
-    int* array3 = fba_malloc(&fba, 10 * sizeof(int));
-    printf("*array = 0x%x\n", *((uint32_t*)((void*) array - MEM_OFFSET)));
-    printf("*array2 = 0x%x\n", *((uint32_t*)((void*) array2 - MEM_OFFSET)));
+    char* string1 = fba_malloc(&fba, 10 * sizeof(char));
+    char* string2 = fba_malloc(&fba, 11 * sizeof(char));
+    char* string3 = fba_malloc(&fba, 15 * sizeof(char));
+    char* string4 = fba_malloc(&fba, 12 * sizeof(char));
+    char* string5 = fba_malloc(&fba, 10 * sizeof(char));
+    char* string6 = fba_malloc(&fba, 10 * sizeof(char));
+    strncpy(string2, "test", fba_get_size(&fba, string2));
+    strncpy(string3, "string", fba_get_size(&fba, string3));
+    printf("string1 = %lu\n", string1);
+    printf("string2 = %lu\n", string2);
+    printf("string3 = %lu\n", string3);
+    printf("string4 = %lu\n", string4);
+    printf("string5 = %lu\n", string5);
+    printf("string6 = %lu\n", string6);
+    printf("*string1 = 0x%x\n", *((uint32_t*)((void*) string1 - MEM_OFFSET)));
+    printf("*string2 = 0x%x\n", *((uint32_t*)((void*) string2 - MEM_OFFSET)));
+    printf("*string3 = 0x%x\n", *((uint32_t*)((void*) string3 - MEM_OFFSET)));
+    printf("*string4 = 0x%x\n", *((uint32_t*)((void*) string4 - MEM_OFFSET)));
+    printf("string2 = %s\n", string2);
+    printf("string3 = %s\n", string3);
+    fba_free(&fba, string2);
+    fba_free(&fba, string3);
+//    printf("*string1 = 0x%x\n", *((uint32_t*)((void*) string1 - MEM_OFFSET)));
+//    printf("*string2 = 0x%x\n", *((uint32_t*)((void*) string2 - MEM_OFFSET)));
+//    printf("*string3 = 0x%x\n", *((uint32_t*)((void*) string3 - MEM_OFFSET)));
+//    printf("*string4 = 0x%x\n", *((uint32_t*)((void*) string4 - MEM_OFFSET)));
+    string6 = fba_malloc(&fba, 27 * sizeof(char));
+    strncpy(string6, "This is a sample string", fba_get_size(&fba, string6));
+    printf("string6 using string2 address = %s\n", string2);
+    printf("string6 = %lu\n", string6);
     
     return 0;
 }
